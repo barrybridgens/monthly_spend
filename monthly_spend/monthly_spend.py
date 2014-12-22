7c770bc9d00c0341cca8f935d2414bb1bd26db7b
Barry Bridgens <barry@bridgens.me.uk>
2014-12-22 18:51:29 +0000

    Initial implementation and tests

--- monthly_spend/monthly_spend.py
+++ monthly_spend/monthly_spend.py
@@ -1,13 +1,28 @@
 # Monthly spend class
 
+import operator
+
 class MonthlySpend:
 
     def __init__(self):
         self.data = []
+        self.full_list = []
 
 
     def read_input_file(self):
         self.data = [line.rstrip() for line in open('input.txt')]
+
+
+    def parse_data_entry(self, data_entry):
+        ret = 'ERROR'
+
+        entry = data_entry.split(' ', 2)
+
+        if (len(entry) == 3):
+            if ((int(entry[0]) >= 1) and (int(entry[0]) <= 12)):
+                ret = entry
+
+        return(ret)
 
 
     def for_month(self, month_number, valid_pattern):
@@ -19,3 +34,12 @@ class MonthlySpend:
                     ret = True
 
         return(ret)
+
+
+    def create_full_list(self):
+        for month in range(1, 13):
+            for d in self.data:
+                pd = self.parse_data_entry(d)
+                if (self.for_month(month, pd[1])):
+                    self.full_list.append([month, int(pd[0]), pd[2]])
+        self.full_list.sort(key = operator.itemgetter(0, 1))
--- tests/monthly_spend_tests.py
+++ tests/monthly_spend_tests.py
@@ -16,8 +16,11 @@ def test_for_month():
     ms = MonthlySpend()
 
     assert_equal(ms.for_month(1, '10000000000'), False)
+    assert_equal(ms.for_month(1, '1000000000000'), False)
     assert_equal(ms.for_month(0, '111111111111'), False)
     assert_equal(ms.for_month(1, '100000000000'), True)
+    assert_equal(ms.for_month(12, '000000000001'), True)
+    assert_equal(ms.for_month(5, '000010000000'), True)
     assert_equal(ms.for_month(1, '000000000000'), False)
 
 def test_read_input_file():
@@ -29,3 +32,30 @@ def test_read_input_file():
     assert_equal(ms.data[0], '1 111111111111 Test bill 1')
     assert_equal(ms.data[1], '11 101010101010 This is my gas bill')
     assert_equal(ms.data[2], '9 001001001001 Electric Bill')
+
+def test_parse_data_entry():
+    ms = MonthlySpend()
+
+    assert_equal(ms.parse_data_entry('0 111111111111 test data'), 'ERROR')
+    assert_equal(ms.parse_data_entry('1 data'), 'ERROR')
+    assert_equal(ms.parse_data_entry('1 111111111111 test'), ['1', '111111111111','test'])
+    assert_equal(ms.parse_data_entry('1 111111111111 test data'), ['1', '111111111111','test data'])
+    assert_equal(ms.parse_data_entry('11 111111111111 test data'), ['11', '111111111111','test data'])
+
+def test_create_full_list():
+    ms = MonthlySpend()
+    ms.read_input_file()
+    ms.create_full_list()
+
+    assert_equal(ms.full_list[0], [1, 1, 'Test bill 1'])
+    assert_equal(ms.full_list[1], [1, 11, 'This is my gas bill'])
+    assert_equal(ms.full_list[2], [2, 1, 'Test bill 1'])
+    assert_equal(ms.full_list[3], [3, 1, 'Test bill 1'])
+    assert_equal(ms.full_list[4], [3, 9, 'Electric Bill'])
+    assert_equal(ms.full_list[5], [3, 11, 'This is my gas bill'])
+    assert_equal(ms.full_list[6], [4, 1, 'Test bill 1'])
+    assert_equal(ms.full_list[7], [5, 1, 'Test bill 1'])
+    assert_equal(ms.full_list[8], [5, 11, 'This is my gas bill'])
+    assert_equal(ms.full_list[9], [6, 1, 'Test bill 1'])
+    assert_equal(ms.full_list[10], [6, 9, 'Electric Bill'])
+    assert_equal(ms.full_list[11], [7, 1, 'Test bill 1'])
